f235055a9ea2d74268a11e618d94e9936d57835a
hustf <hustf@users.noreply.github.com>
2023-03-19 08:53:12 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.32

---
+++ src/ReplSpotifyPlayer.jl
@@ -0,0 +1,5 @@
+module ReplSpotifyPlayer
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ReplSpotifyPlayer
+using Test
+
+@testset "ReplSpotifyPlayer.jl" begin
+    # Write your tests here.
+end
